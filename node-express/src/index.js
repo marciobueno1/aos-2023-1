a8b8caa9b306ce45083d92f3c9ff39dc458b7e23
Márcio Bueno <marcio.bueno@unicap.br>
2023-04-10 19:24:41 -0300

    Utilização de MODULAR ROUTES do express

--- node-express/src/index.js
+++ node-express/src/index.js
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
+
+import models from "./models";
+import routes from "./routes";
 
 console.log("Hello Node.js project.");
 console.log(process.env.MY_SECRET);
@@ -8,9 +12,35 @@ console.log(process.env.MY_SECRET);
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
+});
+
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  return res.send("Received a GET HTTP method");
+});
+
+app.post("/", (req, res) => {
+  return res.send("Received a POST HTTP method");
+});
+
+app.put("/", (req, res) => {
+  return res.send("Received a PUT HTTP method");
+});
+
+app.delete("/", (req, res) => {
+  return res.send("Received a DELETE HTTP method");
 });
 
 const port = process.env.PORT || 3000;
---
+++ node-express/src/routes/user.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  return res.send(Object.values(req.context.models.users));
+});
+
+router.get("/:userId", (req, res) => {
+  return res.send(req.context.models.users[req.params.userId]);
+});
+
+router.post("/", (req, res) => {
+  return res.send("POST HTTP method on user resource");
+});
+
+router.put("/:userId", (req, res) => {
+  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
+});
+
+router.delete("/:userId", (req, res) => {
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
+});
+
+export default router;
